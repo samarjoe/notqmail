f574065202647086b0d317ac0c084ce1a85f843a
Alan Post <adp@prgmr.com>
2019-07-12 22:41:28 -0600

    remove EXTERNAL_TODO define.
    Remove EXTERNAL_TODO define from the C code and the Makefile,
    except for ifndef sections.
    
    Add EXTTODO to FILES, hier.c, and install-big.c. Remove EXTTODO-INFO.
    
    Remove trailing space.
    
    Modification to ext_todo-20030105.patch.

--- qmail-send.c
+++ qmail-send.c
@@ -1441,7 +1441,6 @@ fd_set *rfds;
 
 /* this file is too long ------------------------------------- EXTERNAL TODO */
 
-#ifdef EXTERNAL_TODO
 stralloc todoline = {0};
 char todobuf[2048];
 int todofdin;
@@ -1458,7 +1457,7 @@ void todo_init()
   flagtodoalive = 1;
   /* sync with external todo */
   if (write(todofdout, "S", 1) != 1) tododied();
-  
+
   return;
 }
 
@@ -1505,7 +1504,7 @@ void todo_del(char* s)
     log1("warning: qmail-send unable to understand qmail-todo\n");
     return;
  }
- 
+
  len = scan_ulong(s,&id);
  if (!len || s[len]) {
   log1("warning: qmail-send unable to understand qmail-todo\n");
@@ -1530,7 +1529,7 @@ fd_set *rfds;
   int r;
   char ch;
   int i;
-  
+
   if (!flagtodoalive) return;
   if (!FD_ISSET(todofdin,rfds)) return;
 
@@ -1573,7 +1572,6 @@ fd_set *rfds;
   }
 }
 
-#endif
 
 /* this file is too long ---------------------------------------------- MAIN */
 
@@ -1640,9 +1638,7 @@ void reread()
    log1("alert: unable to reread controls: unable to switch to home directory\n");
    return;
   }
-#ifdef EXTERNAL_TODO
  write(todofdout, "H", 1);
-#endif
  regetcontrols();
  while (chdir("queue") == -1)
   {
@@ -1707,12 +1703,8 @@ int main(void)
  todo_init();
  cleanup_init();
 
-#ifdef EXTERNAL_TODO
  while (!flagexitasap || !del_canexit() || flagtodoalive)
-#else
- while (!flagexitasap || !del_canexit())
-#endif
- {
+  {
    recent = now();
 
    if (flagrunasap) { flagrunasap = 0; pqrun(); }
